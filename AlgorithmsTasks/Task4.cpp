7bf5b28c047365c703467b1f8c45b5cfd7a7c4ee
k-o-white <116183482+k-o-white@users.noreply.github.com>
2022-11-22 20:56:06 +0400

    Create Task4.cpp

---
+++ AlgorithmsTasks/Task4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int main()
+{
+  //-100, -50, -1, 1, 2, 3, 4
+  //-100, -50, -5, 1, 10, 15
+  std::vector <int> numbers = {-100, -50, -1, 1, 2, 3, 4};
+
+  int posInd = 0;
+  while (posInd < numbers.size() && numbers[posInd] < 0)
+    ++posInd; 
+  int negInd = posInd - 1;
+
+  while (negInd >= 0 || posInd < numbers.size()) 
+  {
+    if (negInd >= 0 && (posInd == numbers.size() || numbers[posInd] > -numbers[negInd]))
+      std::cout << numbers[negInd--] << " ";
+    else 
+      std::cout << numbers[posInd++] << " ";
+  }
+  std::cout << std::endl;
+}
